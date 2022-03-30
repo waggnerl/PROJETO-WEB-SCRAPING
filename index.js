5530b3c60b78e565d05d1bdf07b1165783a4c69f
wagner lira <wagnermflira@gmail.com>
2022-03-30 18:38:13 -0300

    Puppeteer - Levantamento Inicial de Dados

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const puppeteer = require('puppeteer');
 
-
 //=========================
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('example.com/');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto('https://www.google.com/search?q=melhor+provedor+de+internet+por+estado&rlz=1C1GCEU_pt-BRBR999BR999&sxsrf=APq-WBtjH01KwkekbWnj9svrKpJ2WxleAg%3A1648671626258&ei=irtEYpmTD7vM1sQPr9yy8Ao&ved=0ahUKEwiZgIm11O72AhU7ppUCHS-uDK4Q4dUDCA4&uact=5&oq=melhor+provedor+de+internet+por+estado&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsANKBAhBGABKBAhGGABQ0QZY0BNgtRVoAXAAeACAAboBiAHtD5IBBDAuMTKYAQCgAQHIAQjAAQE&sclient=gws-wiz');
+  const rankPovedor = await page.evaluate(() => {
+    return {
+      urlRankProvedor : document.querySelector('.truncation-information').href
+    }
+  });
+  await page.goto(rankPovedor.urlRankProvedor);
+  const rankPovedorEstado = await page.evaluate(() => {
+    return {
+      urlrankPovedorEstado : document.querySelectorAll('.text > ul')
+    }
+  });
+
+  console.log(rankPovedorEstado.urlrankPovedorEstado)
+
+
+  //await page.screenshot({ path: 'example.png' });
 
   await browser.close();
-})();
+})(); 
